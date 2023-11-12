94520b3eafcc182de557c8c91fc2633d09a3d1df
Rafael <147251953+Suitzz@users.noreply.github.com>
2023-11-12 19:10:52 +0100

    Translate app files

---
+++ script.js
@@ -0,0 +1,40 @@
+function translateText() {
+    const textInput = document.getElementById('textInput').value;
+    const languageInput = document.getElementById('languageInput').value;
+
+    // Simulate translation on the client side using Google Translate API
+    translate_text(textInput, languageInput);
+}
+
+function translate_text(text, target_language) {
+    const resultText = document.getElementById('resultText');
+    const apiKey = 'AIzaSyALAlpM1CRZx2hFbvJpZS8atF8ogldn9t4'; // Replace with your actual API key
+
+    // Perform translation using Google Translate API
+    const apiUrl = `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`;
+    
+    fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            q: text,
+            target: target_language,
+        }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        if (data.data && data.data.translations) {
+            resultText.innerText = `Original: ${text}\nTranslated: ${data.data.translations[0].translatedText}`;
+        } else {
+            console.error('Translation Error:', data.error.message);
+            resultText.innerText = `Translation Error: ${data.error.message}`;
+        }
+    })
+    .catch((error) => {
+        console.error('Fetch Error:', error);
+        resultText.innerText = 'Translation Error';
+    });
+}
