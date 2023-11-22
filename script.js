95eb262a100843f16b03a972708daf9a7ac68499
Rafael <147251953+Suitzz@users.noreply.github.com>
2023-11-22 13:57:38 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,20 @@
-function translateText() {
+function selectLanguage(event) {
+    event.preventDefault();
     const textInput = document.getElementById('textInput').value;
     const languageSelect = document.getElementById('languageSelect');
-    const selectedLanguage = languageSelect.options[languageSelect.selectedIndex].value;
+    const targetLanguage = languageSelect.options[languageSelect.selectedIndex].value;
 
     // Simulate translation on the client side using Google Translate API
-    translate_text(textInput, selectedLanguage);
+    translateText(textInput, targetLanguage);
 }
 
-function translate_text(text, target_language) {
+function translateText(text, targetLanguage) {
     const resultText = document.getElementById('resultText');
-    const apiKey = 'AIzaSyALAlpM1CRZx2hFbvJpZS8atF8ogldn9t4'; // Replace with your actual API key
+    const apiKey = 'AIzaSyALAlpM1CRZx2hFbvJpZS8atF8ogldn9t4';
 
     // Perform translation using Google Translate API
     const apiUrl = `https://translation.googleapis.com/language/translate/v2?key=${apiKey}`;
-    
+
     fetch(apiUrl, {
         method: 'POST',
         headers: {
@@ -22,13 +23,13 @@ function translate_text(text, target_language) {
         },
         body: JSON.stringify({
             q: text,
-            target: target_language,
+            target: targetLanguage,
         }),
     })
     .then(response => response.json())
     .then(data => {
         if (data.data && data.data.translations) {
-            resultText.innerText = `${data.data.translations[0].translatedText}`;
+            resultText.innerText = data.data.translations[0].translatedText;
         } else {
             console.error('Translation Error:', data.error.message);
             resultText.innerText = `Translation Error: ${data.error.message}`;
@@ -39,3 +40,30 @@ function translate_text(text, target_language) {
         resultText.innerText = 'Translation Error';
     });
 }
+
+function changeLanguage(languageCode) {
+    const languageSelect = document.getElementById('languageSelect');
+    const flagButtons = document.getElementsByClassName('flagButton');
+
+    // Set the selected language in the dropdown
+    for (let i = 0; i < languageSelect.options.length; i++) {
+        if (languageSelect.options[i].value === languageCode) {
+            languageSelect.selectedIndex = i;
+            break;
+        }
+    }
+
+    // Trigger translation
+    selectLanguage(new Event('click'));
+
+    // Prevent the default behavior of the click event on flag buttons
+    event.preventDefault();
+}
+
+// Flag buttons event listeners
+document.getElementById('flagButtons').addEventListener('click', function (event) {
+    if (event.target.classList.contains('flagButton')) {
+        const languageCode = event.target.getAttribute('data-lang');
+        changeLanguage(languageCode);
+    }
+});
